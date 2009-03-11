0eb4f4c23d310eab40e9c4b2dfcbe4ade0e67d76
Dan Korostelev <nadako@gmail.com>
2009-03-11 16:07:11 +0000

    Split LocalPermission to zope.localpermission.

---
+++ README.txt
@@ -0,0 +1,2 @@
+This package implements local persistent permissions for zope.security that
+can be added and registered per site.
---
+++ setup.py
@@ -0,0 +1,60 @@
+##############################################################################
+#
+# Copyright (c) 2009 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.localpermission package
+
+$Id$
+"""
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(name='zope.localpermission',
+      version = '3.7.0dev',
+      author='Zope Corporation and Contributors',
+      author_email='zope-dev@zope.org',
+      description='Local Persistent Permissions for zope.security',
+      long_description=(
+          read('README.txt')
+          + '\n\n' +
+          read('CHANGES.txt')
+          ),
+      keywords = "zope security persistent local permission",
+      classifiers = [
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Zope Public License',
+          'Programming Language :: Python',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Topic :: Internet :: WWW/HTTP',
+          'Framework :: Zope3'],
+      url='http://pypi.python.org/pypi/zope.localpermission',
+      license='ZPL 2.1',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['zope'],
+      install_requires=['setuptools',
+                        'ZODB3',
+                        'zope.component',
+                        'zope.i18nmessageid',
+                        'zope.interface',
+                        'zope.location',
+                        'zope.security',
+                        ],
+      include_package_data = True,
+      zip_safe = False,
+      )
